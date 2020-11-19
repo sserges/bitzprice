d4565cf127348745e35fdcdc72c4543c9d12924c
sserges <agounkeserge@gmail.com>
2020-11-19 17:46:29 +0000

    NavBar added

---
+++ components/Navbar.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+
+const Navbar = () => (
+  <div>
+    <ul>
+      <li>
+        <Link href='/'>
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href='/about'>
+          <a>About</a>
+        </Link>
+      </li>
+    </ul>
+
+    <style jsx>{`
+      ul {
+        background: #333;
+        color: #fff;
+        list-style: none;
+        display: flex;
+      }
+
+      ul li {
+        font-size: 18px;
+        margin-right: 20px;
+      }
+    `}</style>
+  </div>
+);
+
+export default Navbar;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+
+const About = () => (
+  <div>
+    <Navbar />
+    <h1>About BitzPrice</h1>
+    <p>Application to view Bitcoin prices</p>
+  </div>
+);
+
+export default About;
